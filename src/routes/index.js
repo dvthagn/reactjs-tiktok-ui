5c0cc9bd46f934bf9184bf4887c8906b3d87eaf1
Thang dinh <thangdinh14102002@gmail.com>
2023-06-08 22:33:25 +0700

    config routes/layouts

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+//Layouts
+import HeaderOnly from '~/components/Layout/HeaderOnly';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+
+//Public Routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
