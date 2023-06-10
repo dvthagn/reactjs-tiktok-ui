483d96dfbc422df6a39561b2f25866f0206e0a3d
Thang dinh <thangdinh14102002@gmail.com>
2023-06-10 22:24:31 +0700

    UI header-2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/5c98890331f20987545f2483aacf443d.jpeg?x-expires=1686582000&x-signature=PZiPuKiMd1pCtWhtuNWtKd%2BxyHQ%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen van a</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>Nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
